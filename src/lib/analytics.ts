e7a432a9f8ba281c91aca64d9cb755b7b79db372
Web Dev Cody <webdevcody@gmail.com>
2024-04-07 09:26:52 -0400

    adding conditional to only send events on prod

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -1,4 +1,5 @@
 export async function sendEvent(key: string) {
+  if (process.env.NEXT_PUBLIC_IS_LOCAL) return;
   await fetch("https://projectplannerai.com/api/events", {
     method: "POST",
     headers: {
